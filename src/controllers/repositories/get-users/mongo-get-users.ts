880b45bfecce9f3e89db76a8e0d56f1e699ec298
Gustavo Bezerra <devgustavobezerra@gmail.com>
2024-10-11 21:15:05 -0300

    feat: add id to user

--- src/controllers/repositories/get-users/mongo-get-users.ts
+++ src/controllers/repositories/get-users/mongo-get-users.ts
@@ -1,15 +1,14 @@
+import { MongoClient } from "../../../database/mongo";
 import { User } from "../../../models/user";
 import { IGetUsersRepository } from "../../get-users/protocols";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
-    return [
-      {
-        firstName: "Gustavo",
-        lastName: "Bezerra",
-        email: "gustavobezerrase@gmail.com",
-        password: "1234",
-      },
-    ];
+    const users = await MongoClient.db
+      .collection<User>("users")
+      .find()
+      .toArray();
+
+    return users;
   }
 }
