ed59160c082bcf2e1a7c2356d76148d674d9ebff
Gustavo Bezerra <devgustavobezerra@gmail.com>
2024-10-11 21:20:09 -0300

    feat: ensure GetUsersMongoRepository gets users from mongo

--- src/controllers/repositories/get-users/mongo-get-users.ts
+++ src/controllers/repositories/get-users/mongo-get-users.ts
@@ -5,10 +5,13 @@ import { IGetUsersRepository } from "../../get-users/protocols";
 export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
     const users = await MongoClient.db
-      .collection<User>("users")
+      .collection<Omit<User, "id">>("users")
       .find()
       .toArray();
 
-    return users;
+    return users.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }));
   }
 }
